d74dba387020703f23325f0ad6a0d991020073f2
Arnaud Mazeyrat <kpn41@hotmail.fr>
2023-02-20 18:44:58 +0100

    début création page d'accueil

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import React from 'react'
 import { Router, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import About from './pages/About/index'
+import Home from './pages/Home/index'
+/*import About from './pages/About/index'
 import Product from './pages/Product/index'
 import Header from './components/Header/index'
 import Footer from './components/Footer/index'
-import Error from './components/Error/index'
+import Error from './components/Error/index'*/
 
 function App() {
- return (
-  <Router>
-   <Header />
-
-   <Route exact path="/">
-    <Home />
-   </Route>
-   <Route path="/fiche-logement">
-    <Product />
-   </Route>
-   <Route path="/a-propos">
-    <About />
-   </Route>
-   <Route path="/footer">
-    <Footer />
-   </Route>
-   <Route>
-    <Error />
-   </Route>
-
-   <Footer />
-  </Router>
- )
+    return (
+        <Router>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Router>
+    );
 }
 
 export default App
